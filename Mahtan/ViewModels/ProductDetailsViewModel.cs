e0edf12134e5d5dfea68887c08279e215f224d7c
hadi nazaripoor <hadi.nazaripoor@yahoo.com>
2023-01-08 23:06:31 +0330

    gg

--- Mahtan/ViewModels/ProductDetailsViewModel.cs
+++ Mahtan/ViewModels/ProductDetailsViewModel.cs
@@ -1,6 +1,10 @@
-﻿namespace Mahtan.ViewModels
+﻿using Mahtan.Models;
+
+namespace Mahtan.ViewModels
 {
     public class ProductDetailsViewModel
     {
+        public Product Product { get; set; }
+        public IEnumerable<Product> MyProperty { get; set; }
     }
 }
